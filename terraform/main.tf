5177ea381348d0a8633e3dedea01121705cacb8f
Aman <amanharsora543@gmail.com>
2025-02-11 16:00:38 +0000

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  region = us-east-1  # Variable for flexibility
+  region = "us-east-1"  # Ensure region is enclosed in double quotes
 }
 
-# Use the default security group in your VPC for simplicity
 resource "aws_instance" "t2_medium_instance" {
   ami           = "ami-085ad6ae776d8f09c"  # Use the desired AMI for your region
   instance_type = "t2.medium"              # Instance type
-  key_name      = project2             # Use your SSH key name
+  key_name      = "project2"                # Ensure key_name is in quotes
 
-  # No custom security group, we will use default
-  vpc_security_group_ids = [aws_security_group.default.id]
+  # No custom security group, we will use default security group
+  vpc_security_group_ids = ["default"]  # Use default security group ID, no need to refer to aws_security_group.default.id
 
   tags = {
     Name = "k8s-instance"
@@ -17,7 +16,7 @@ resource "aws_instance" "t2_medium_instance" {
 
   # Instance metadata options to reduce delays on startup
   metadata_options {
-    http_tokens = "required"
+    http_tokens   = "required"
     http_endpoint = "enabled"
   }
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region to launch the resources"
   type        = string
-  default     = "us-east-1"  # Change this to the region you want
+  default     = "us-east-1"  # You can change this default to any region you prefer
 }
 
 variable "key_name" {
   description = "The name of the SSH key pair"
   type        = string
-  default     = "project2"   # Replace this with your actual key name
+  default     = "project2"   # Replace this with your actual key name (ensure it matches your key)
 }
